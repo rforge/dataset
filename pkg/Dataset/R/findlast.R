01ed24051bda28b86e10cbe30cc506b2fc7aeaaa
erousseaux <erousseaux@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-01 16:10:24 +0000

    Add find.last.value, switch to 0.2.44
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/dataset@127 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Dataset/R/findlast.R
+++ pkg/Dataset/R/findlast.R
@@ -122,3 +122,159 @@ findlast <- function (data, from, to){
 #   )
 # )
 
+which.this.value <- function(x, value)
+{
+  equal.to.this.value <- function(e) { e == value }
+  
+  sapply(x, equal.to.this.value)
+}
+# fv.test1 <- c(0,0,1,0,0,NA,NA,NA,NA,NA,NA,NA,NA)
+# which.this.value(fv.test1, 1)
+
+exists.this.value <- function(x, value) # if the value exists, return the last index of the column where the value is, if the value doesn't exist, return -1, if all NA, return NA
+{
+  y <- which.this.value(x, value)
+  
+  if(all(is.na(y))) {
+    return(list('value' = NA, 'index' = NA))
+  }
+  
+  if(is.na(any(y))) {
+    return(list('value' = -1, 'index' = max(which(!y))))
+  }
+  
+  return(list('value' = 1, 'index' = max(which(y))))
+}
+# fv.test1 <- c( 0, 0, 1, 0, 0,NA,NA,NA,NA,NA,NA,NA,NA)
+# fv.test2 <- c(NA,NA, 0, 0, 1, 1, 1,NA,NA,NA,NA,NA,NA)
+# fv.test3 <- c(NA,NA,NA, 0,NA, 1, 1, 1,NA,NA,NA,NA,NA)
+# fv.test4 <- c(NA,NA,NA,NA,NA,NA,NA,NA, 0, 0, 0, 0, 0)
+# fv.test5 <- c(NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA)
+# exists.this.value(fv.test1, 1)
+# exists.this.value(fv.test2, 1)
+# exists.this.value(fv.test3, 1)
+# exists.this.value(fv.test4, 1)
+# exists.this.value(fv.test5, 1)
+
+find.last.value <- function (x, value)
+{
+  return(data.frame(t(mapply(exists.this.value, as.data.frame(t(x)), value)), row.names = 1:nrow(x)))
+}
+# fv.data.example <- data.frame(
+#   'unemployment99' = c(0,NA,NA,NA,NA),
+#   'unemployment00' = c(0,NA,NA,NA,NA),
+#   'unemployment01' = c(1,0,NA,NA,NA),
+#   'unemployment02' = c(0,0,0,NA,NA),
+#   'unemployment03' = c(0,1,NA,NA,NA),
+#   'unemployment04' = c(NA,1,1,NA,NA),
+#   'unemployment05' = c(NA,1,1,NA,NA),
+#   'unemployment06' = c(NA,NA,1,NA,NA),
+#   'unemployment07' = c(NA,NA,NA,0,NA),
+#   'unemployment08' = c(NA,NA,NA,0,NA),
+#   'unemployment09' = c(NA,NA,NA,0,NA),
+#   'unemployment10' = c(NA,NA,NA,0,NA),
+#   'unemployment11' = c(NA,NA,NA,0,NA)
+# )
+# find.last.value(fv.data.example, 1)
+
+find.value.last <- function (data, from, to, value = 1)
+{
+  if (missing(data))
+    stop("'data' argument is missing")
+  if (missing(from))
+    stop("'from' argument is missing")
+  l <- length(from)
+  if (l <= 1) {
+    stop("'from' argument must have at leat 2 elements")
+  }
+  data.from <- data[, from]
+  last <- find.last.value(data.from, value)
+  if (missing(to)) {
+    return(last)
+  }
+  else {
+    out <- vector(length(names(to)), mode = "list")
+    names(out) <- names(to)
+    for (i in names(to)) {
+      names.current <- to[[i]]
+      if (length(names.current) != l) {
+        message(paste("The element '", i, "' of the 'to' argument has not the same number of column than the 'from' argument.",
+                      sep = ""))
+        message(paste(i, "element of 'to':", paste(names.current,
+                                                   collapse = ", ")))
+        message(paste("'from' argument:", paste(from,
+                                                collapse = ", ")))
+        stop("Bad parameters")
+      }
+      data.current <- data[, names.current]
+      last.current <- get.last.valid(data.current, last[,'index'])
+      out[[i]] <- last.current
+    }
+    out <- as.data.frame(out)
+    return(out)
+  }
+}
+# fv.data.example <- data.frame(
+#   'unemployment99' = c(0,NA,NA,NA,NA),
+#   'unemployment00' = c(0,NA,NA,NA,NA),
+#   'unemployment01' = c(1,0,NA,NA,NA),
+#   'unemployment02' = c(0,0,0,NA,NA),
+#   'unemployment03' = c(0,1,NA,NA,NA),
+#   'unemployment04' = c(NA,1,1,NA,NA),
+#   'unemployment05' = c(NA,1,1,NA,NA),
+#   'unemployment06' = c(NA,NA,1,NA,NA),
+#   'unemployment07' = c(NA,NA,NA,0,NA),
+#   'unemployment08' = c(NA,NA,NA,0,NA),
+#   'unemployment09' = c(NA,NA,NA,0,NA),
+#   'unemployment10' = c(NA,NA,NA,0,NA),
+#   'unemployment11' = c(NA,NA,NA,0,NA),
+#   'age99' = c(15,NA,NA,NA,NA),
+#   'age00' = c(16,NA,NA,NA,NA),
+#   'age01' = c(17,20,NA,NA,NA),
+#   'age02' = c(18,21,23,NA,NA),
+#   'age03' = c(19,22,NA,NA,NA),
+#   'age04' = c(NA,23,25,NA,NA),
+#   'age05' = c(NA,24,26,NA,NA),
+#   'age06' = c(NA,NA,27,NA,NA),
+#   'age07' = c(NA,NA,NA,29,NA),
+#   'age08' = c(NA,NA,NA,30,NA),
+#   'age09' = c(NA,NA,NA,31,NA),
+#   'age10' = c(NA,NA,NA,32,NA),
+#   'age11' = c(NA,NA,NA,33,NA),
+#   'education99' = c(8,NA,NA,NA,NA),
+#   'education00' = c(8,NA,NA,NA,NA),
+#   'education01' = c(8,3,NA,NA,NA),
+#   'education02' = c(8,3,2,NA,NA),
+#   'education03' = c(8,4,NA,NA,NA),
+#   'education04' = c(NA,4,2,NA,NA),
+#   'education05' = c(NA,4,2,NA,NA),
+#   'education06' = c(NA,NA,3,NA,NA),
+#   'education07' = c(NA,NA,NA,7,NA),
+#   'education08' = c(NA,NA,NA,7,NA),
+#   'education09' = c(NA,NA,NA,7,NA),
+#   'education10' = c(NA,NA,NA,8,NA),
+#   'education11' = c(NA,NA,NA,8,NA)
+# )
+# find.value.last(
+#   data = fv.data.example,
+#   from = 1:13,
+#   value = 1
+# )
+# find.value.last(
+#   data = fv.data.example,
+#   from = 1:13,
+#   to = list(
+#     'age' = 14:26,
+#     'education' = 27:39
+#   ),
+#   value = 1
+# )
+# find.value.last(
+#   data = fv.data.example,
+#   from = 1:13,
+#   to = list(
+#     'age' = 14:26,
+#     'education' = 27:39
+#   ),
+#   value = 0
+# )
